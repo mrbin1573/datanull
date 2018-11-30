698824971740715f4d67ad9d59aed1b051ed45c1
MrBinUsername <1573406407@qq.com>
2018-11-30 18:56:53 +0800

    去除jQuer依赖

--- js/dataNull.js
+++ js/dataNull.js
@@ -1,53 +1,64 @@
-;(function ($) {
-  $.fn.dataNull = function (options) {
-    if(typeof options.img != 'undefined'){
-      console.log(typeof options.img )
-      if(typeof options.img !== 'string'){
-        options.img = 'http://hackbinimg.luokangyuan.com/datanull/datanull(' +options.img+ ').png';
+;(function (win, undefined) {
+  function dataNull(options) {
+    var $dataNullBox = document.getElementById(options.eleId);
+    var settings = {}
+    if (typeof options.img != 'undefined') {
+      if (typeof options.img !== 'string') {
+        options.img = 'http://hackbinimg.luokangyuan.com/datanull/datanull(' + options.img + ').png';
       }
-    }else{
+    } else {
       options.img = 'http://hackbinimg.luokangyuan.com/datanull/datanull(1).png';
     }
+    // 设置默认参数
+    settings.text = typeof (options.text) == 'undefined' ? '暂无数据' : options.text;
+    settings.width = typeof (options.width) == 'undefined' ? '100%' : options.width;
+    settings.height = typeof (options.height) == 'undefined' ? '100%' : options.height;
+    settings.imgWidth = typeof (options.imgWidth) == 'undefined' ? '100px' : options.imgWidth;
+    settings.top = typeof (options.top) == 'undefined' ? '50px' : options.top;
+    settings.img = typeof (options.img) == 'undefined' ? 'http://hackbinimg.luokangyuan.com/datanull/datanull(1).png' : options.img;
+    settings.bgColor = typeof (options.bgColor) == 'undefined' ? '#fff' : options.bgColor;
+    settings.txtColor = typeof (options.txtColor) == 'undefined' ? '#3e3e3e' : options.txtColor;
+    settings.txtSize = typeof (options.txtSize) == 'undefined' ? '24px' : options.txtSize;
+    settings.txtWeight = typeof (options.txtWeight) == 'undefined' ? 'bold' : options.txtWeight;
+    settings.outerWidth = typeof (options.outerWidth) == 'undefined' ? '' : options.outerWidth;
+    settings.outerHeight = typeof (options.outerHeight) == 'undefined' ? '' : options.outerHeight;
 
-    var settings = $.extend({
-      'text': '暂无数据',
-      'width': '100%',
-      'height': '100%',
-      'imgWidth': '100px',
-      'top': '50px',
-      'img': 'http://hackbinimg.luokangyuan.com/datanull/datanull(1).png',
-      'bgColor': '#fff !important',
-      'txtAlign': 'center',
-      'txtColor': '#3e3e3e',
-      'txtWeight': 'bold',
-      'txtSize': '24px'
-    }, options);
-    return this.each(function () {
-      var $dataNullEle = $('<div></div>');
-      var $textEl = $('<div style="margin-top:10px"></div>');
-      var $imgEl = $('<img/>');
-      $dataNullEle.css({
-        'width': settings.width,
-        'height': settings.height,
-        'margin-top': settings.top,
-        'background-color': settings.bgColor,
-        'text-align': settings.txtAlign,
-      })
+    var $dataNullEle = document.createElement('div');
+    var $textEl = document.createElement('div');
+    var $imgEl = document.createElement('img');
 
-      $textEl.text(settings.text).css(
-        {
-          'font-weight': settings.txtWeight,
-          'font-size': settings.txtSize,
-          'color': settings.txtColor
-        }
-      );
+    // 空数据容器
+    $dataNullEle.style.width = settings.width;
+    $dataNullEle.style.height = settings.height;
+    $dataNullEle.style.marginTop = settings.top;
+    $dataNullEle.style.backgroundColor = settings.bgColor;
+    $dataNullEle.style.textAlign = 'center';
+    
+    console.log(settings.width)
+    //文本
+    $textEl.innerText = settings.text;
+    $textEl.style.fontWeight = settings.txtWeight;
+    $textEl.style.fontSize = settings.txtSize;
+    $textEl.style.color = settings.txtColor;
+    
+    // 图片
+    $imgEl.src = settings.img;
+    $imgEl.style.width = settings.imgWidth;
+    
+    // 外部容器宽高
+    $dataNullBox.style.overflow = 'hidden';
+    if (typeof settings.outerHeight != 'undefined') {
+      $dataNullBox.style.height = settings.height;
+    }
+    if (typeof settings.outerWidth != 'undefined') {
+      $dataNullBox.style.width = settings.outerWidth;
+    }
 
-      $imgEl.attr('src', settings.img).css({
-        'width': settings.imgWidth
-      });
+    $dataNullEle.innerHTML = '';
+    $dataNullEle.appendChild($imgEl);
+    $dataNullEle.appendChild($textEl);
+    $dataNullBox.appendChild($dataNullEle);
+  }
 
-      $dataNullEle.append($imgEl, $textEl)
-      $(this).append($dataNullEle);
-    });
-  };
-})(jQuery);
+  window.dataNull = dataNull;
+})(window);
--- readme.md
+++ readme.md
@@ -1,5 +1,9 @@
 ## dataNull插件参数
 
+> 插件会清空应用元素的内容
+
+[实例](https://mrbin1573.github.io/datanull/)
+
 | 参数     | 说明                   | 默认值  | 类型 |
 | ------   | ------                 | ------  | ------ |
 | text     | 显示的文字              | 暂无数据 | string |
@@ -7,9 +11,14 @@
 | height   | 高度(不改变外层容器大小) | 100%    |string |
 | imgWidth | 图片宽度                | 100px   |string |
 | top      | 距离容器上部距离         | 50px    |string |
-| img      | 显示图片，默认值为1~10（必须是数字），也可传图片路径（string）   | 1     |number/string |
+| img      | 显示图片，默认值为1~10（必须是数字），也可传图片路径（string）|1|number/string|
 | bgColor  | 背景颜色                | #fff    |string |
-| txtAlign | 对齐方式                | center  |string |
 | txtColor | 文字颜色                | #3e3e3e |string |
 | txtWeight| 文字粗细                | bold    |string |
-| txtSize  | 字体大小                | 24px    |string |
+| outerWidth| 外部容器宽度            | 空     |string |
+| outerHeight| 外部容器高度            | 空     |string |
+
+> width/height是指在 选中容器内部 的宽高
+
+
+> outerWidth/outerHeight是改变 选中容器 宽高
